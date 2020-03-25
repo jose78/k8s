c3e0ebf2bb2343e3accc2b49e23aef631b240183
Claver <jose.clavero.anderica@gmail.com>
2020-03-25 01:26:26 +0100

    created consumer

---
+++ module_consumer/src/test/java/com/github/demo/comsumer/services/ConsumerSvc.java
@@ -0,0 +1,20 @@
+package com.github.demo.comsumer.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class ConsumerSvc {
+
+    private final Logger logger = LoggerFactory.getLogger(ConsumerSvc.class);
+
+    @KafkaListener(topics = "users", groupId = "group_id")
+    public void consume(String message) throws IOException {
+        logger.info(String.format("#### -> Consumed message -> %s", message));
+    }
+
+}
