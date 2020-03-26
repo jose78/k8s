1d7a39b62e50f3a1718daf796d6ddefed5f30517
Claver <jose.clavero.anderica@gmail.com>
2020-03-26 01:49:43 +0100

    refactor: location of main app and append new rest entrypoint

--- k8s/monitor_kafka/module_producer/src/main/java/com/github/demo/controller/DemoController.java
+++ k8s/monitor_kafka/module_producer/src/main/java/com/github/demo/controller/DemoController.java
@@ -2,10 +2,11 @@ package com.github.demo.controller;
 
 import com.github.demo.services.ProducerSvc;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 
 @RestController
@@ -14,6 +15,21 @@ public class DemoController {
 
     @Autowired
     private ProducerSvc producerSvc;
+
+    @Value(value = "${app.version}")
+    private String  version;
+
+    @Value(value = "${app.env}")
+    private String  env;
+
+    @GetMapping("/status")
+    public Map<String , String> status() {
+        Map<String , String> map=  new TreeMap<String,String>();
+        map.put("Status" , "ok");
+        map.put("version", version);
+        map.put("env", env);
+        return map;
+    }
 
     @PostMapping(value = "/publish")
     public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
